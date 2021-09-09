7cc1eaee161ca67c063c80805e07824c776bb7d0
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-09 00:25:13 -0300

    Initial Commit

---
+++ swiftUI-000/swiftUI_000App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_000App.swift
+//  swiftUI-000
+//
+//  Created by Luiz Carlos da Silva Araujo on 09/09/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
